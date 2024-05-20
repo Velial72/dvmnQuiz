cbac6d6172f092f39a611ef0d8b26c864bd3318c
Velial72 <126498671+Velial72@users.noreply.github.com>
2024-05-20 21:26:02 +0500

    Update get_question.py
    внес правки согласно ревью

--- get_question.py
+++ get_question.py
@@ -2,27 +2,24 @@ import os
 import json
 import random
 from pathlib import Path
+from environs import Env
 
 from redis_client import redis_client
 
-BASE_DIR = Path(__file__).resolve().parent
-json_file_path = os.path.join(BASE_DIR / 'quiz-questions/questions_and_answers.json')
-
 
 #получаем случайный вопрос из json файла
-def get_question():
+def get_question(json_file_path):
     with open(json_file_path, "r", encoding="utf-8") as json_file:
         qa_dict = json.load(json_file)
 
     random_key = random.choice(list(qa_dict.keys()))
     random_value = qa_dict[random_key]
 
-    print("Ответ на вопрос:", random_value)
     return random_key
 
 
 # Получение ответа по ключу (вопросу)
-def get_correct_answer(question: str, flag=True):
+def get_correct_answer(question: str, json_file_path, flag=True):
     with open(json_file_path, 'r', encoding="utf-8") as f:
         data = json.load(f)
     answer = data[question].lower()
@@ -37,15 +34,8 @@ def get_correct_answer(question: str, flag=True):
             short_answer = answer
         if short_answer.startswith('"') and short_answer.endswith('"'):
             short_answer = short_answer[1:-1]
-        print(short_answer)
         return short_answer
-    else:
-        return answer
-
-
-#добавляем вопрос в базу
-def add_question_to_user(user_id: int, question: str):
-    redis_client.hset(f"user:{user_id}", "questions", question)
+    return answer
 
 
 #проверяем был ли вопрос
